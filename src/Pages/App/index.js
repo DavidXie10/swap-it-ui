f9b4e88d64999d049b7d6f6728e842f9ef61656a
DavidXie10 <davidxieli@gmail.com>
2022-05-29 23:38:43 -0600

    Passed App script and test script to Pages App Page

---
+++ src/Pages/App/index.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/*<Route path="login" element={<Login />} />*/}
+                {/*<Route path="/" element={<Home />} />*/}
+            </Routes>
+        </BrowserRouter>  
+    );
+}
