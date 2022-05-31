a8b3f29d03bbd9ef36b124f71ff8343d3f6d794d
DavidXie10 <davidxieli@gmail.com>
2022-05-30 21:28:55 -0600

    Implemented logo component and Showcase component for testing

---
+++ src/Components/Showcase/index.js
@@ -0,0 +1,7 @@
+import Logo from '../Logo'
+
+export default function index() {
+    return (
+        <Logo />
+    )
+}
--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -1,9 +1,11 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Showcase from "../../Components/Showcase"
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Showcase />}/> 
                 {/*<Route path="login" element={<Login />} />*/}
                 {/*<Route path="/" element={<Home />} />*/}
             </Routes>
