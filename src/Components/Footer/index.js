3b27a2d0284915cbcc60c72ca8eeac06d9e8a2c3
DavidXie10 <davidxieli@gmail.com>
2022-06-01 15:43:13 -0600

    Changed Footer paragraphs

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -5,15 +5,18 @@ export default function Footer({height}) {
     return (
         <div className={`w-full bg-[#2E2F2F] `}>
             <div className={`flex sm:px-6 md:px-8 lg:px-16  items-center py-16`}>
-                <div className="w-1/2 flex" >
+                <div className="w-1/3 flex" >
                     <Link to='/catalog'>
                         <Logo height='h-15'/>
                     </Link>                
                 </div>
-                <div className="w-1/2 flex flex-col justify-center ">   
-                    <p className="text-white flex justify-end text-2xl right-0">San Pedro, San José</p>
-                    <a href="tel:+50624518181" className="text-white text-2xl flex justify-end">2451-8181</a>
-                    <a href="tel:+50687930495" className="text-white text-2xl flex justify-end">8793-0495</a>
+                <div className="w-1/3"></div>
+                <div className="w-1/3"></div>
+
+                <div className="flex flex-col justify-center ">   
+                    <p className="text-white flex justify-end text-2xl justify-end0">San Pedro, San José</p>
+                    <p className="text-white text-2xl flex justify-end">2451-8181</p>
+                    <p className="text-white text-2xl flex justify-end">8793-0495</p>
                     <a href="mailto:swapitecci@gmail.com" className="text-white text-2xl flex justify-end">swapitecci@gmail.com</a>
                 </div>
             </div>
