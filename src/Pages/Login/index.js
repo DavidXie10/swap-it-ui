3551d28968175ca57900665ec5c9e103a6039c29
DavidXie10 <davidxieli@gmail.com>
2022-06-05 17:44:52 -0600

    Changed heights using vh

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -7,10 +7,10 @@ export default function Login() {
 
     return (
         <div className={`pt-[20vh] ${containerClases} bg-[#2e2f2f]  h-screen`}>
-            <div className={`${containerClases} h-20`}>
+            <div className={`${containerClases} h-[20vh]`}>
                 <Logo height='h-15'/>
             </div>
-            <div className='h-10'> 
+            <div className='h-[15vh]'> 
                 <div className={`${containerClases} w-[350px]`}>
                     <Input id='user' placeholder='Ingrese su usuario' type='text' />
                 </div>
@@ -18,12 +18,12 @@ export default function Login() {
                     <Input id='password' placeholder='Ingrese su contraseña' type='password' />
                 </div>
             </div>
-            <div className={`${containerClases} hidden`}>
+            <div className={`${containerClases} hidden h-[5vh]`}>
                 <p className={`text-red-500`} id="errorMessage">
                     Usuario o contraseña incorrectas
                 </p>
             </div>
-            <div className={`${containerClases} h-50`}>
+            <div className={`${containerClases} h-[20vh]`}>
                 <Button width='w-[350px]' height='h-[40px]' label='Ingresar' />
             </div>
         </div>
