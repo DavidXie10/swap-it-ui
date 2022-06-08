dc7e2e0df4748eed507acd4eaf1b4f24f47f2808
DavidXie10 <davidxieli@gmail.com>
2022-06-08 00:13:37 -0600

    Implemented post login async validation

---
+++ src/Slices/user/requests/postLogin.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const postLogin = createAsyncThunk('users/postLogin', async(credentials) => {
+    const loginFetch = await fetch('http://localhost:3001/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: credentials.username,
+            password: credentials.password,
+        }),
+    });
+
+    const userData = await loginFetch.json();
+
+    if (loginFetch.status === 200){
+        return userData;
+    }else{
+        return {
+            error: true,
+            message: userData.error.message,
+        }
+    }
+})
+
+export const onPostLoginFulfilled = (state, action) => {
+    if (action.payload.error){
+        state.isLoggedIn = false;
+        state.user = null;
+        state.errorMessage = action.payload.message;
+    }else{
+        state.isLoggedIn = true;
+        state.user = action.payload;
+    }
+}
+
+export const onPostLoginRejected = (state) => {
+    state.isLoggedIn = false;
+    state.user = null;
+}
