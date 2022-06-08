003d5f93a6e8d7da7909b2b781fcd023341af920
DavidXie10 <davidxieli@gmail.com>
2022-06-08 00:14:29 -0600

    Implemented user slice to handle login logic with redux

---
+++ src/Slices/user/userSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { onPostLoginFulfilled, onPostLoginRejected, postLogin } from "./requests/postLogin";
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        user: null,
+        isLoggedIn: false,
+        errorMessage: "",
+    },
+    reducers: {
+        logout: (state) => {
+            state.user = null;
+            state.isLoggedIn = false;
+        }
+    },
+    extraReducers (builder){
+        builder
+            .addCase(postLogin.fulfilled, onPostLoginFulfilled)
+            .addCase(postLogin.rejected, onPostLoginRejected)
+    }
+})
+
+export const { logout } = userSlice.actions;
+
+export default userSlice.reducer;
