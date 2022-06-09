02cc93725cecc738ec24ab14c21d14fdffb00d85
DavidXie10 <davidxieli@gmail.com>
2022-06-08 21:29:44 -0600

    Implemented persistance in app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 import './index.css';
 import App from './Pages/App/';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 
+const persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
+        <PersistGate loading={null} persistor={persistor}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </PersistGate>
     </Provider>
 );
 
