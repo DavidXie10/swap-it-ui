ca1952a54e0e8f4fd4d38e508b0d19e90ef44e77
DavidXie10 <davidxieli@gmail.com>
2022-06-08 18:23:15 -0600

    Drag and drop working

--- src/Components/DragAndDrop/index.js
+++ src/Components/DragAndDrop/index.js
@@ -4,35 +4,45 @@ import { FileUploader } from "react-drag-drop-files";
 const fileTypes = ["JPEG", "PNG", "GIF", "JPG"];
 
 export default function DragAndDrop() {
-  const [fileList, setFileList] = useState();
-  const handleChange = (file) => {
-    //let fileTemp = file;
-    //fileTemp[]
-    //console.log(fileList);
-    //let tempList = file;
-    //tempList.append(inputFile);
-    //setFile(tempList);
-    //let fileListTemp = fileList + file;
-    //setFileList(fileListTemp);
-    if (!fileList) {
-        setFileList(file);
-    } else {
-        let fileListTemp = fileList + file;
-        //fileListTemp.append(file)
-        setFileList(fileListTemp);
+    const [fileList, setFileList] = useState({
+        length: 0
+    });
+
+    const handleChange = (inputFiles) => {
+        let updateFileList = {
+            ...fileList
+        };
+
+        let inputFilesCounter = 0;
+        
+        for(let counter = updateFileList.length; inputFilesCounter < inputFiles.length; ++inputFilesCounter, ++counter){
+            updateFileList[counter] = inputFiles[inputFilesCounter];
+        }
+
+        updateFileList.length += inputFiles.length;
+        setFileList(updateFileList);
+    };
+
+    const getFiles = () => {
+        let filenames = '';
+
+        for(let file = 0; file < fileList.length; ++file){
+            filenames += `[${fileList[file].name}] `;
+        }
+
+        return filenames;
     }
-    console.log(fileList);
-  };
-  return (
-    <div className="App">
-      <FileUploader
-        multiple={true}
-        handleChange={handleChange}
-        name="file"
-        types={fileTypes}
-      />
-      {/* <p>{fileList ? `File name: ${fileList[0].name}` : "no files uploaded yet"}</p>
-      {fileList && fileList[0] && <img src={URL.createObjectURL(fileList[0])} alt="test" />} */}
-    </div>
-  );
+
+    return (
+        <>
+            <FileUploader
+                multiple={true}
+                handleChange={handleChange}
+                name="file"
+                types={fileTypes}
+            />
+            { <p>{fileList.length > 0 ? `Uploaded file names: ${getFiles()}` : "no files uploaded yet"}</p>}
+
+        </>
+    );
 }
