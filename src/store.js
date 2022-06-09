d153b3fe1cdb7f8ad60d942d1607e89e2ed31ad0
DavidXie10 <davidxieli@gmail.com>
2022-06-08 21:30:53 -0600

    Adapted store to persist data on local storage

--- src/store.js
+++ src/store.js
@@ -1,14 +1,23 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import appSlice from "./Slices/app/appSlice";
-import userSlice from "./Slices/user/userSlice";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from "redux-persist";
+import appSlice from './Slices/app/appSlice';
+import userSlice from './Slices/user/userSlice';
 
 const reducers = combineReducers({
     app: appSlice,
     user: userSlice,
 })
 
+const rootPersistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(rootPersistConfig, reducers);  
+
 const store = configureStore({
-    reducer: reducers,
+    reducer: persistedReducer,
 });
 
 export default store;
