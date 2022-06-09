e208b328757bd57058fa7394288c852f467642e0
DavidXie10 <davidxieli@gmail.com>
2022-06-09 08:40:58 -0600

    Adapted app routes for private routes, Login is the root

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PrivateRoute from "../../Components/PrivateRoute";
 import Showcase from "../../Components/Showcase"
 import Login from "../Login";
 
@@ -6,9 +7,10 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Showcase />}/> 
-                <Route path="login" element={<Login />} />
-                {/*<Route path="login" element={<Login />} />*/}
+                <Route path="/" element={<Login />}/> 
+                <Route element={<PrivateRoute />}>
+                    <Route path="/showcase" element={<Showcase />}/> 
+                </Route>
                 {/*<Route path="/" element={<Home />} />*/}
             </Routes>
         </BrowserRouter>
