8e8c61a44dd0db8ed718a261faba9bc8df229dcb
DavidXie10 <davidxieli@gmail.com>
2022-06-11 00:18:02 -0600

    Added default classes to label component

--- src/Components/Label/index.js
+++ src/Components/Label/index.js
@@ -1,7 +1,7 @@
 export default function Label ({
-    backgroundcolor, textcolor, width, height, text, size
+    backgroundcolor, textcolor, width, height, text, size, alignText
 }) {
     return (
-        <p className={`${backgroundcolor} ${textcolor} ${width || 'w-8'} ${height || 'h-8'} ${size} text-center font-bold`}> {text} </p>
+        <p className={`${backgroundcolor || ''} ${textcolor || 'text-black'} ${width || 'w-8'} ${height || 'h-8'} ${size || 'text-4xl'} ${alignText || 'text-left'} font-bold`}> {text} </p>
     )
 }
