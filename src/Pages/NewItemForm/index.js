ba7ab8a79c6e64e1e64ffa199b8e73eaeb6615b6
DavidXie10 <davidxieli@gmail.com>
2022-06-13 23:40:35 -0600

    Added description and corrected location

--- src/Pages/NewItemForm/index.js
+++ src/Pages/NewItemForm/index.js
@@ -6,18 +6,19 @@ import Input from '../../Components/Input'
 import Button from '../../Components/Button'
 import DragAndDrop from '../../Components/DragAndDrop'
 import CloseButton from '../../Components/CloseButton'
+import Success from '../../Components/Success'
 import { useDispatch, useSelector } from 'react-redux'
 import { toggleSuccess } from '../../Slices/item/itemSlice'
-import { Link } from 'react-router-dom'
 
-export default function NewArticleForm() {
+export default function NewItemForm() {
     const [item, setItem] = useState({
         name: '',
-        location: '',
-        desiredSwapItems: '',
+        wishlist: '',
         acquisitionDate: '',
+        description: '',
         itemState: -1,
         category: -1,
+        location: -1,
         photosUrl: [],
     });
 
@@ -32,7 +33,9 @@ export default function NewArticleForm() {
     const [itemStateErrorMessage, setItemStateErrorMessage] = useState('');
     const [locationErrorMessage, setLocationErrorMessage] = useState('');
     const [categoryErrorMessage, setCategoryErrorMessage] = useState('');
-    const [swapItemsErrorMessage, setSwapItemsErrorMessage] = useState('');
+    const [wishlistErrorMessage, setWishlistErrorMessage] = useState('');
+    const [descriptionErrorMessage, setDescriptionErrorMessage] = useState('');
+
 
     const handleUploadedFile = (inputFiles) => {
         if (inputFiles.length + fileList.length <= 3){
@@ -106,48 +109,58 @@ export default function NewArticleForm() {
         return images;
     }
 
-    const validateInputs = () => {
-        return item.name.trim() === '' || item.acquisitionDate.trim() === '' || item.location.trim() === '' || item.desiredSwapItems.trim() === '' || item.itemState === -1 || item.category === -1 || fileList.length === 0;
-    }
-
     const isValidForm = () => {
+        let isValid = true;
         if(item.name.trim() === ''){
+            isValid &&= false;
             setNameErrorMessage('Por favor ingrese el nombre del artículo');
         }else{
             setNameErrorMessage('');
         }
         if(item.acquisitionDate.trim() === ''){
+            isValid &&= false;
             setAcquisitionDateErrorMessage('Por favor ingrese la fecha de adquisición');
         }else{
             setAcquisitionDateErrorMessage('');
         }
-        if(item.location.trim() === ''){
-            setLocationErrorMessage('Por favor ingrese la ubicación');
+        if(item.location === -1){
+            isValid &&= false;
+            setLocationErrorMessage('Por favor seleccione la provincia de residencia');
         }else{
             setLocationErrorMessage('');
         }
-        if(item.desiredSwapItems.trim() === ''){
-            setSwapItemsErrorMessage('Por favor ingrese los artículos por los que quiere intercambiar');
+        if(item.wishlist.trim() === ''){
+            isValid &&= false;
+            setWishlistErrorMessage('Por favor ingrese los artículos por los que quiere intercambiar');
         }else{
-            setSwapItemsErrorMessage('');
+            setWishlistErrorMessage('');
         }
         if(item.itemState === -1){
+            isValid &&= false;
             setItemStateErrorMessage('Por favor seleccione el estado del artículo');
         }else{
             setItemStateErrorMessage('');
         }
         if(item.category === -1){
+            isValid &&= false;
             setCategoryErrorMessage('Por favor seleccione una categoría');
         }else{
             setCategoryErrorMessage('');
         }
         if(fileList.length === 0){
+            isValid &&= false;
             setFileErrorMessage('Por favor suba al menos una imagen');
         }else{
             setFileErrorMessage('');
         }
+        if(item.description.trim() === ''){
+            isValid &&= false;
+            setDescriptionErrorMessage('Por favor ingrese la descripción del artículo');
+        }else{
+            setDescriptionErrorMessage('');
+        }
 
-        return validateInputs();
+        return isValid;
     }
 
     const success = useSelector(
@@ -160,24 +173,7 @@ export default function NewArticleForm() {
         <div className='flex min-h-screen flex-col justify-between'>
             <Header />
             {success ? (
-                <>
-                    <div className='p-8 w-full sm:px-6 md:px-8 lg:px-16'>
-                        <div className="sm:px-6 md:px-8 lg:px-16 flex p-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
-                            <svg className="inline flex-shrink-0 mr-3 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
-                            <div>
-                                <span className="font-medium">Artículo creado exitosamente.</span> 
-                            </div>
-                        </div>
-                        
-                    </div>
-                    <div className='sm:px-6 md:px-8 lg:px-16 flex p-4'>
-                        <button className={`flex bg-[#51e5ff] text-white w-80 h-12 rounded-md font-bold text-lg`}> 
-                            <Link to='/catalog' className='flex w-full h-full flex-col justify-center'>
-                                Regresar al catálogo
-                            </Link>
-                        </button>
-                    </div>
-                </>
+                <Success message={'Artículo creado exitosamente'} buttonMessage={'Regresar al catálogo'}/>
         ) : (
             <form className='p-8 w-full sm:px-6 md:px-8 lg:px-16 mb-2'>
                 <div className='flex w-full'>
@@ -187,7 +183,7 @@ export default function NewArticleForm() {
                     <div className='lg:flex md:flex lg:flex-nowrap md:flex-nowrap w-full sm:flex-wrap'>
                         <div className='lg:flex md:flex w-full sm:flex-wrap mb-4'>
                             <div className='flex w-full'>
-                                <Input id={'articleName'} placeholder='Título del artículo' width='w-full' marginBottom={'mb-0'} onChange={(evt) => handleChange('name', evt.target.value)}/>             
+                                <Input id={'name'} placeholder='Ingrese el nombre del artículo' width='w-full' marginBottom={'mb-0'} onChange={(evt) => handleChange('name', evt.target.value)}/>             
                             </div>
                             <div className='pt-2'>
                                 {nameErrorMessage && <p className="text-red-500 font-bold">{nameErrorMessage}</p>}
@@ -206,8 +202,25 @@ export default function NewArticleForm() {
                     <div className='lg:flex md:flex lg:flex-nowrap md:flex-nowrap w-full sm:flex-wrap'>
                         <div className='lg:flex md:flex w-full sm:flex-wrap mb-4'>
                             <div className='flex w-full'>
-                                <Input id={'location'} placeholder='Ubicación' width='w-full' marginBottom={'mb-0'} onChange={(evt) => handleChange('location', evt.target.value)}/>         
+                                <select
+                                    id={'location'}
+                                    value={item.location}
+                                    className="w-full h-10 px-4 rounded-md focus:outline-none text-lg font-semibold border border-solid border-gray-600 bg-white appearance-none"
+                                    onChange={(evt) => {
+                                        handleChange("location", parseInt(evt.target.value));
+                                    }}
+                                >
+                                    <option disabled className="w-full" label="Seleccione la provincia donde se encuentra" value={-1} ></option>
+                                    <option className="w-full" label="San José" value={1}></option>
+                                    <option className="w-full" label="Alajuela" value={2}></option>
+                                    <option className="w-full" label="Cartago" value={3}></option>
+                                    <option className="w-full" label="Heredia" value={4}></option>
+                                    <option className="w-full" label="Guanacaste" value={5}></option>
+                                    <option className="w-full" label="Puntarenas" value={6}></option>
+                                    <option className="w-full" label="Limón" value={7}></option>
+                                </select>     
                             </div>    
+
                             <div className='pt-2'>
                                 {locationErrorMessage && <p className="text-red-500 font-bold">{locationErrorMessage}</p>}
                             </div>    
@@ -216,10 +229,11 @@ export default function NewArticleForm() {
                         <div className='lg:flex md:flex w-full sm:flex-wrap mb-4'>
                             <div className='flex w-full'>
                                 <select 
+                                    value={item.itemState}
                                     className='w-full h-10 px-4 rounded-md focus:outline-none text-lg font-semibold border border-solid border-gray-600 bg-white appearance-none' 
                                     onChange={(evt) => handleChange('itemState', parseInt(evt.target.value))}
                                 >
-                                    <option disabled selected className='w-full' label='Seleccione un estado' value={-1}></option>
+                                    <option disabled className='w-full' label='Seleccione el estado de su artículo' value={-1}></option>
                                     <option className='w-full' label='Nuevo' value={1}></option>
                                     <option className='w-full' label='Usado' value={2}></option>
                                 </select>
@@ -254,12 +268,20 @@ export default function NewArticleForm() {
                     </div>
                     <div className='lg:flex md:flex lg:flex-nowrap md:flex-nowrap w-full sm:flex-wrap'>
                         <div className='lg:flex md:flex w-full sm:flex-wrap'>
-                            <textarea rows={3} id={'swapItems'} className='w-full px-4 rounded-md focus:outline-none text-lg font-semibold border border-solid border-gray-600 pt-2' placeholder='¿Qué buscas para intercambiar?' onChange={(evt) => handleChange('desiredSwapItems', evt.target.value)} />
+                            <textarea rows={3} id={'description'} className='w-full px-4 rounded-md focus:outline-none text-lg font-semibold border border-solid border-gray-600 pt-2' placeholder='Ingrese la descripción del artículo' onChange={(evt) => handleChange('description', evt.target.value)} />
                         </div>
                     </div>
                     <div className='pt-2 mb-4'>
-                            {swapItemsErrorMessage && <p className="text-red-500 font-bold">{swapItemsErrorMessage}</p>}
+                        {descriptionErrorMessage && <p className="text-red-500 font-bold">{descriptionErrorMessage}</p>}
+                    </div>
+                    <div className='lg:flex md:flex lg:flex-nowrap md:flex-nowrap w-full sm:flex-wrap'>
+                        <div className='lg:flex md:flex w-full sm:flex-wrap'>
+                            <textarea rows={3} id={'wishlist'} className='w-full px-4 rounded-md focus:outline-none text-lg font-semibold border border-solid border-gray-600 pt-2' placeholder='Ingrese lo que busca para intercambiar' onChange={(evt) => handleChange('wishlist', evt.target.value)} />
                         </div>
+                    </div>
+                    <div className='pt-2 mb-4'>
+                        {wishlistErrorMessage && <p className="text-red-500 font-bold">{wishlistErrorMessage}</p>}
+                    </div>
                     <div className='lg:flex md:flex sm:flex lg:flex-nowrap md:flex-nowrap w-full sm:flex-wrap'>
                         <div className='lg:flex md:flex sm:flex lg:flex-nowrap md:flex-nowrap w-full sm:flex-wrap'>
                             <DragAndDrop handleChange={handleUploadedFile} fileList={fileList} />
@@ -277,10 +299,9 @@ export default function NewArticleForm() {
                         {showUploadedImages()}
                     </div>}
                     <div className='lg:flex md:flex sm:flex lg:flex-nowrap md:flex-nowrap w-full sm:flex-wrap justify-end mt-4'>
-
                         <Button type={'button'} label='Agregar' textcolor='text-white' width='w-56' height='h-12' onClick={() => {
-                            if(isValidForm()){
-                                console.log('Todo bien');
+                            if(!isValidForm()){
+                                console.log('Todo mal');
                             }else{
                                 dispatch(toggleSuccess());
                             }
