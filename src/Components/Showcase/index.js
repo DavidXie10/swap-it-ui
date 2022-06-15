4b9f47b127e259d0516bc632370c9d8126d96e7a
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-06-15 10:54:03 -0600

    MyItem component completed

--- src/Components/Showcase/index.js
+++ src/Components/Showcase/index.js
@@ -2,11 +2,11 @@
 //51E5FF
 import MyItem from '../MyItem';
 
-
+//image, title, state, address, acquisition, searchFor, backgroundcolor, onClickEdit, onClickDelete
 export default function Showcase() {
     return (
         <>
-            <MyItem></MyItem>
+            <MyItem title='Bicicleta con rodines' state='Usado' address='San Jose' acquisition='11/06/22' searchFor='bicicleeta sin rodines' backgroundcolor={'bg-slate-200'}></MyItem>
         </>
     )
 }
