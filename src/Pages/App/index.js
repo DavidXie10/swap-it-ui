6f256f5251f4b160439373c2c38bba90f6bd2097
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-06-15 11:51:00 -0600

    Created Item Selected Page

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -2,12 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrivateRoute from "../../Components/PrivateRoute";
 import Showcase from "../../Components/Showcase"
 import Login from "../Login";
+import ItemSelected from "../ItemSelected";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Showcase />}/> 
+                <Route path="/" element={<ItemSelected />}/> 
                 <Route element={<PrivateRoute />}>
                     <Route path="/showcase" element={<Showcase />}/> 
                 </Route>
