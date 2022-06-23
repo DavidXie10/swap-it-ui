f81a0043163e16cd9983299956bc99b139efb54c
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-06-23 08:23:42 -0600

    An item selected page its getting responsive

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -4,11 +4,17 @@ import Showcase from "../../Components/Showcase"
 import Login from "../Login";
 import ItemSelected from "../ItemSelected";
 
+// //images, title, state, address, acquisition, description, onClickExchange
+// const url_1 = 'https://www.gravatar.com/avatar/1d42ea6e005a4160211f7b1957ce0a09/?default=&s=64';
+// let img = document.createElement("img");
+// img.src = url_1;
+
+
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<ItemSelected />}/> 
+                <Route path="/" element={<ItemSelected title={'Bicicleta con rodines'} owner={'Juani'} state={'usado'} address={'San José'} acquisition={'20/05/2021'} description={'La bici esta como nueva, tiene conos, aros nuevos, recién pintada de rojo, si no me va a dar su alma no me ofrezca nada'} />}/> 
                 <Route element={<PrivateRoute />}>
                     <Route path="/showcase" element={<Showcase />}/> 
                 </Route>
--- src/Pages/ItemSelected/index.js
+++ src/Pages/ItemSelected/index.js
@@ -1,31 +1,36 @@
 import BackButton from "../../Components/BackButton";
 import Button from "../../Components/Button";
 import Label from "../../Components/Label";
+import Footer from "../../Components/Footer";
+import Header from "../../Components/Header";
 
+//sm:px-6 md:px-8 lg:px-16
+
+//Estoy haceindo responsive para que se haga pequeño. Falta que las imagenes en la versión pequeña tomen sus 8 filas y que en la verisón grande el botón de Intercambiar este ubicado correctamente y que este centrado el back boton en la versión pequeña y la grande.
 export default function MyItem ({
-    images, title, state, address, acquisition, description, onClickEdit, onClickDelete
+    images, title, owner, state, address, acquisition, description, onClickExchange
 }) {
     return (
-        <div>
-            {/* TODO: Agregar Header y Footer */}
-            {/* TODO: Agregar margen respecto a bordes */}
-            <div className="flex flex-row">
-                <Label text={"Artículo seleccionado"} width={'basis-3/4'} textposition={'text-left'} font={'font-bold'} size={'text-4xl'}></Label>
-                <BackButton width={'basis-1/4'}></BackButton>
+        <div className='flex min-h-screen flex-col justify-between'>
+            <Header />
+            <div className="sm:px-6 md:px-8 lg:px-16">
+                <div className="flex flex-row justify-between w-full mb-16">
+                    <Label text={"Artículo seleccionado"} width={'basis-3/4'} textposition={'text-left'} font={'font-bold'} size={'lg:text-4xl md:text-4xl sm:text-2xl'}></Label>
+                    <BackButton></BackButton>
+                </div>
+                <div className="grid lg:grid-rows-[8] md:grid-rows-[8] sm:grid-rows-[16] grid-cols-5 gap-2 mb-16">
+                    {/* Image carousel */}
+                    { images ? <img className="row-[span_8_/_span_8] lg:col-span-2 md:col-span-2 sm:col-span-5" src={images} alt={"Foto del artículo a intercambiar"}/> : <p className="row-[span_8_/_span_8] lg:col-span-2 md:col-span-2 sm:col-span-5">Fotos no encontradas</p>}
+                    <Label text={title || "Title not found"} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={'font-bold'} textposition={'text-left'} size={'lg:text-4xl md:text-4xl sm:text-3xl'}></Label>
+                    <Label text={`Propietario: ${owner || "Owner not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
+                    <Label text={`Estado: ${state || "State not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
+                    <Label text={`Ubicación: ${address || "Address not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
+                    <Label text={`Fecha de adquisición: ${acquisition || "Acquisition not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
+                    <Label text={`Descripción: ${description || "Desciption not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
+                    <Button textcolor='text-white' width='lg:w-[180px] md:w-[180px] sm:col-span-5' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Intercambiar' onClick={onClickExchange || ''}/>
+                </div>
             </div>
-            {/* <b/>
-            <b/> */}
-            <div className="grid grid-rows-6 grid-cols-5 gap-2 ">
-                {/* Image carousel */}
-                { images ? <img className="row-span-6 col-span-2 text-center" src={URL.createObjectURL(images)} alt={"Foto del artículo a intercambiar"}/> : <p className="row-span-6 col-span-2 text-center mb-4">Fotos no encontradas</p>}
-                <Label text={title || "Title not found"} width={'col-span-3'} height={'row-span-1'} font={'font-bold'} textposition={'text-left'} size={'text-4xl'}></Label>
-                <Label text={`Estado: ${state || "State not found"}`} width={'col-span-3'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
-                <Label text={`Ubicación: ${address || "Address not found"}`} width={'col-span-3'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
-                <Label text={`Fecha de adquisición: ${acquisition || "Acquisition not found"}`} width={'col-span-3'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
-                <Label text={`Descripción: ${description || "Desciption not found"}`} width={'col-span-3'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
-                {/* <Button width={'col-span-3'} height={'row-span-1'} label='Editar' onClick={onClickEdit || ''}/> */}
-                <Button textcolor='text-white' width='w-[180px]' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Intercambiar' onClick={onClickEdit || ''}/>
-            </div>
+            <Footer />
         </div>
     )
 }
