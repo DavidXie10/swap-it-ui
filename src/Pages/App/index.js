75cd081d676c931e5e779f619ff073af66f99b1f
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-06-24 19:36:53 -0600

    Added working carousel

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -8,13 +8,13 @@ import ItemSelected from "../ItemSelected";
 // const url_1 = 'https://www.gravatar.com/avatar/1d42ea6e005a4160211f7b1957ce0a09/?default=&s=64';
 // let img = document.createElement("img");
 // img.src = url_1;
-
+const listImages = ['https://www.tresorsdegrece.gr/wp-content/uploads/2018/10/ALAS-Messolongi-sea-salt-crystals-small.jpg', 'https://www.tresorsdegrece.gr/wp-content/uploads/2015/04/Ethnos-COOKBOOK-April-2015-85x85.jpg', 'https://www.tresorsdegrece.gr/wp-content/uploads/2015/04/Ethnos-COOKBOOK-April-2015c-528x450.jpg']
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<ItemSelected title={'Bicicleta con rodines'} owner={'Juani'} state={'usado'} address={'San José'} acquisition={'20/05/2021'} description={'La bici esta como nueva, tiene conos, aros nuevos, recién pintada de rojo, si no me va a dar su alma no me ofrezca nada'} />}/> 
+                <Route path="/" element={<ItemSelected imagesSource={listImages} title={'Bicicleta con rodines'} owner={'Juani'} state={'usado'} address={'San José'} acquisition={'20/05/2021'} description={'La bici esta como nueva, tiene conos, aros nuevos, recién pintada de rojo, si no me va a dar su alma no me ofrezca nada'} />}/> 
                 <Route element={<PrivateRoute />}>
                     <Route path="/showcase" element={<Showcase />}/> 
                 </Route>
--- src/Pages/ItemSelected/index.js
+++ src/Pages/ItemSelected/index.js
@@ -3,13 +3,29 @@ import Button from "../../Components/Button";
 import Label from "../../Components/Label";
 import Footer from "../../Components/Footer";
 import Header from "../../Components/Header";
+import 'tw-elements';
 
 //sm:px-6 md:px-8 lg:px-16
 
-//Estoy haceindo responsive para que se haga pequeño. Falta que las imagenes en la versión pequeña tomen sus 8 filas y que en la verisón grande el botón de Intercambiar este ubicado correctamente y que este centrado el back boton en la versión pequeña y la grande. Probando push ecci.
-export default function MyItem ({
-    images, title, owner, state, address, acquisition, description, onClickExchange
+//Estoy haceindo responsive para que se haga pequeño. Falta que las imagenes en la versión pequeña tomen sus 8 filas y que en la verisón grande el botón de Intercambiar este ubicado correctamente y que este centrado el back boton en la versión pequeña y la grande.
+export default function ItemSelected ({
+    imagesSource, title, owner, state, address, acquisition, description, onClickExchange
 }) {
+    console.log(imagesSource);
+    let first = true;
+    const listImages = imagesSource.map((image) =>
+        {if (first){
+            first = false;
+            return  <div className="carousel-item active relative float-left w-full">
+                        <img src={image} alt={'Imagen artículo de catálogo'}/>
+                    </div>;
+        } else {
+            return  <div className="carousel-item relative float-left w-full">
+                        <img src={image} alt={'Imagen artículo de catálogo'}/>
+                    </div>;
+        } }
+    );
+
     return (
         <div className='flex min-h-screen flex-col justify-between'>
             <Header />
@@ -19,18 +35,41 @@ export default function MyItem ({
                     <BackButton></BackButton>
                 </div>
                 <div className="grid lg:grid-rows-[8] md:grid-rows-[8] sm:grid-rows-[16] grid-cols-5 gap-2 mb-16">
-                    {/* Image carousel */}
-                    { images ? <img className="row-[span_8_/_span_8] lg:col-span-2 md:col-span-2 sm:col-span-5" src={images} alt={"Foto del artículo a intercambiar"}/> : <p className="row-[span_8_/_span_8] lg:col-span-2 md:col-span-2 sm:col-span-5">Fotos no encontradas</p>}
+                    <div id="carouselExampleControls" className="carousel slide relative row-[span_8_/_span_8] lg:col-span-2 md:col-span-2 sm:col-span-5  border border-b-neutral-400" data-bs-ride="carousel">
+                        <div class="carousel-inner relative w-full overflow-hidden flex items-center justify-center text-center">
+                            { listImages }
+                        </div>
+                        <button
+                            class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                            type="button"
+                            data-bs-target="#carouselExampleControls"
+                            data-bs-slide="prev"
+                        >
+                            <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button
+                            class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                            type="button"
+                            data-bs-target="#carouselExampleControls"
+                            data-bs-slide="next"
+                        >
+                            <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+
                     <Label text={title || "Title not found"} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={'font-bold'} textposition={'text-left'} size={'lg:text-4xl md:text-4xl sm:text-3xl'}></Label>
                     <Label text={`Propietario: ${owner || "Owner not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
                     <Label text={`Estado: ${state || "State not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
                     <Label text={`Ubicación: ${address || "Address not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
                     <Label text={`Fecha de adquisición: ${acquisition || "Acquisition not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
                     <Label text={`Descripción: ${description || "Desciption not found"}`} width={'lg:col-span-3 md:col-span-3 sm:col-span-5'} height={'row-span-1'} font={''} textposition={'text-left'} size={'text-md'}></Label>
-                    <Button textcolor='text-white' width='lg:w-[180px] md:w-[180px] sm:col-span-5' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Intercambiar' onClick={onClickExchange || ''}/>
+                    <Button textcolor='text-white' width='lg:col-span-3 md:col-span-3 sm:col-span-5 w-[180px]' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Intercambiar' onClick={onClickExchange || ''}/>
                 </div>
             </div>
             <Footer />
         </div>
     )
 }
+
