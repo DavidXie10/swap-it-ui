cc071043dad6d62b3664c569efad210ffe019cb5
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-06-25 21:50:21 -0600

    Adjusted buttons of Edit and Delete responsiveness

--- src/Components/MyItem/index.js
+++ src/Components/MyItem/index.js
@@ -7,19 +7,21 @@ export default function MyItem ({
     return (
         <div className={`${backgroundcolor || ""}`}>
             <div className={`flex justify-end ${backgroundcolor || ""}`}></div>
-            <div className="grid grid-rows-6 grid-cols-5 gap-2 mb-4 mt-8 mb-8">
+            <div className="grid grid-rows-6 grid-cols-5 gap-1 mt-8 mb-8">
                 <div className={`row-span-6 col-span-2 ${backgroundcolor || ""} flex justify-center`}>
-                    { image ? <img className={`lg:w-[30%] md:w-[70%] sm:w-[61%] `} src={image} alt={"Foto del artículo a intercambiar"}/> : <p className="row-span-6 text-center">Foto no encontrada</p>}
+                    { image ? <img className={`lg:w-[30%] md:w-[70%] sm:w-[80%] `} src={image} alt={"Foto del artículo a intercambiar"}/> : <p className="row-span-6 text-center">Foto no encontrada</p>}
                 </div>
-                <Label text={title || "Title not found"} width={'col-span-2'} height={'row-span-2'} font={'font-bold'} textposition={'text-left'} backgroundcolor={backgroundcolor || ""} ></Label>
-                <Label text={`Estado: ${state || "State not found"}`} width={'col-span-3'} font={''} textposition={'text-left'} backgroundcolor={backgroundcolor || ""}></Label>
-                <Label text={`Ubicación: ${address || "Address not found"}`} width={'col-span-3'} font={''} textposition={'text-left'} backgroundcolor={backgroundcolor || ""}></Label>
-                <Label text={`Fecha de adquisición: ${acquisition || "Acquisition not found"}`} width={'col-span-3'} font={''} textposition={'text-left'} backgroundcolor={backgroundcolor || ""}></Label>
-                <Label text={`Busco: ${searchFor || "SearchFor not found"}`} width={'col-span-3'} font={''} textposition={'text-left'} backgroundcolor={backgroundcolor || ""}></Label>
-                <Button width='w-[180px] col-start-3' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Editar' onClick={onClickEdit || ''}/>
-                <Button width='w-[180px]' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Eliminar' onClick={onClickDelete || ''} backgroundcolor='bg-[#8C8D8D]' textcolor='text-white'/>
+                <Label text={title || "Title not found"} width={'col-span-3'} height={'row-span-2'} font={'font-bold'} textposition={'text-left'} backgroundcolor={backgroundcolor || ""} size={'text-4xl'}></Label>
+                <Label text={`Estado: ${state || "State not found"}`} width={'col-span-3'} height={'row-span-1'} font={''} textposition={'text-left'} backgroundcolor={backgroundcolor || ""}></Label>
+                <Label text={`Ubicación: ${address || "Address not found"}`} width={'col-span-3'} height={'row-span-1'} font={''} textposition={'text-left'} backgroundcolor={backgroundcolor || ""}></Label>
+                <Label text={`Fecha de adquisición: ${acquisition || "Acquisition not found"}`} height={'row-span-1'} width={'col-span-3'} font={''} textposition={'text-left'} backgroundcolor={backgroundcolor || ""}></Label>
+                <Label text={`Busco: ${searchFor || "SearchFor not found"}`} width={'col-span-3'} height={'row-span-1'} font={''} textposition={'text-left'} backgroundcolor={backgroundcolor || ""}></Label>
+                <Button width='lg:w-[180px] md:w-[180px] sm:w-[75px] col-start-3 flex justify-center items-center' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Editar' onClick={onClickEdit || ''}/>
+                <Button width='lg:w-[180px] md:w-[180px] sm:w-[75px] col-start-4 flex justify-center items-center ml-1' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Eliminar' onClick={onClickDelete || ''} backgroundcolor='bg-[#8C8D8D]' textcolor='text-white'/>
             </div>
             <div className={`flex justify-end ${backgroundcolor || ""}`}></div>
         </div>
     )
 }
+
+//width='w-[360px]' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'}
