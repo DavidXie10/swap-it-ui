16014a9dc80e0a03712dd505eda17837ae3ca765
DavidXie10 <davidxieli@gmail.com>
2022-06-26 00:49:38 -0600

    Adapted create item async function with backend implementation

--- src/Slices/item/requests/createItem.js
+++ src/Slices/item/requests/createItem.js
@@ -1,32 +1,38 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const createItem = createAsyncThunk('item/new', async ({item, fileList}) => {
+export const createItem = createAsyncThunk('item/new', async ({item, fileList}, { getState }) => {
+    const state = getState();
     const formData = new FormData();
     
     for(let counter = 0; counter < fileList.fileIdCount; ++counter){
         formData.append('file', fileList[counter]);
     }
     
-/*
     const uploadFetch = await fetch('http://localhost:8000/uploads', {
+        headers: {
+            "Authorization": `Bearer ${state.user.user.token}`,
+        },
         method: 'POST',
         body: formData,
     });
 
     const uploadedData = await uploadFetch.json();
-    item.photosUrl = uploadedData.urls;
+
+    item.photoUrls = uploadedData.uploadedFiles.map((file) => file.url);
+    console.log(item.photoUrls);
 
     const itemFetch = await fetch('http://localhost:8000/items', {
         method: 'POST',
         headers: {
             "Content-type": "application/json",
+            "Authorization": `Bearer ${state.user.user.token}`,
         },
         body: JSON.stringify(item),
     });
 
     const itemData = await itemFetch.json();
     
-    if (itemFetch.status === 200) {
+    if (itemFetch.status === 201) {
         return itemData;
     } else {
         return {
@@ -34,7 +40,4 @@ export const createItem = createAsyncThunk('item/new', async ({item, fileList}) 
             message: itemData.error.message,
         }
     }
-    */
-    console.log(item);
-    return item;
 });
