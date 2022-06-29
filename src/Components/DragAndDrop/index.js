824d2f3afe449c641919c9cafdb5ef5a38fbaac4
DavidXie10 <davidxieli@gmail.com>
2022-06-28 22:41:20 -0600

    Adapted drag and drop to not use DOM access

--- src/Components/DragAndDrop/index.js
+++ src/Components/DragAndDrop/index.js
@@ -1,16 +1,10 @@
-import { useEffect } from "react";
 import { FileUploader } from "react-drag-drop-files";
 
 const FILE_TYPES = ["JPEG", "PNG", "JPG"];
 
-export default function DragAndDrop({handleChange, fileList}) {
-    useEffect(() => {
-        let label = document.getElementsByClassName('sc-bczRLJ')[0];
-        label.setAttribute('style', 'max-width:100%; height:8rem');
-    }, []);
-
+export default function DragAndDrop({handleChange}) {
     return (
-        <div className="h-full w-full">
+        <div className='h-full w-full' id='container_file_uploader'>
             <FileUploader
                 multiple={true}
                 handleChange={handleChange}
