039d2208b30f9f6e695cdcdd16719c82a7eabb3c
DavidXie10 <davidxieli@gmail.com>
2022-06-29 16:52:00 -0600

    Removed error message when logout

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,7 +1,7 @@
 import Logo from '../../Components/Logo'
 import Input from '../../Components/Input'
 import Button from '../../Components/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { postLogin } from '../../Slices/user/requests/postLogin';
@@ -40,16 +40,18 @@ export default function Login() {
 
             <div className={`flex w-full justify-center`}>
                 {errorMessage && <span className="text-red-500 sm:mt-4">{errorMessage}</span>}
-                {localErrorMessage && <span className="text-red-500 sm:mt-4">{localErrorMessage}</span>}
+            </div>
+            <div className={`flex w-full justify-center`}>
+                {localErrorMessage && <span className="text-red-500">{localErrorMessage}</span>}
             </div>
 
             <div className={`${containerClases} lg:h-[20vh] md:h-[20vh] sm:h-[15vh]`}>
                 <Button width='w-[360px]' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Ingresar' onClick={() => {
                     if(email && password){
                         if(password.length < 8) {
-                            setLocalErrorMessage('La contraseña debe contener al menos 8 caracteres.');
+                            setLocalErrorMessage('La contraseña debe contener mínimo 8 caracteres');
                         } else if(!isValidEmail(email)){
-                            setLocalErrorMessage('Ingrese una dirección de correo válida.');
+                            setLocalErrorMessage('Ingrese una dirección de correo válida');
                         } else{
                             setLocalErrorMessage('');
                             dispatch(postLogin({email, password}));
