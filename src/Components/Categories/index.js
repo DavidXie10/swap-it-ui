44ad1e143f021472841e6e3643337e1a8724f380
solvalle <vallesol07@gmail.com>
2022-07-01 10:58:18 -0600

    Deleted category component and added icon color in categories

--- src/Components/Categories/index.js
+++ src/Components/Categories/index.js
@@ -16,22 +16,25 @@ import { useState } from "react";
 
 export default function Categories() 
 {
+    const [selectedCategory, setSelectedCategory] = useState(0);
+
+    const iconColor = (id) => selectedCategory === id?'bg-[#51E5FF]':'bg-[#C4C4C4]';
+
     const categories = [
-        { id:0, name:'Todas', icon: <IoGrid className={`${iconCategoryClasses}`}/> },
-        { id:1, name:'Deportes', icon: <IoAmericanFootball className={`${iconCategoryClasses}`}/> },
-        { id:2, name:'Electrónica', icon: <IoPhonePortraitOutline className={`${iconCategoryClasses}`}/> },
-        { id:3, name:'Hogar', icon: <IoHome className={`${iconCategoryClasses}`}/> },
-        { id:4, name:'Libros', icon: <IoBookSharp className={`${iconCategoryClasses}`}/> },
-        { id:5, name:'Oficina', icon: <IoPrint className={`${iconCategoryClasses}`}/> },
-        { id:6, name:'Películas', icon: <IoFilmSharp  className={`${iconCategoryClasses}`}/> },
-        { id:7, name:'Ropa', icon: <IoShirt className={`${iconCategoryClasses}`}/> },
-        { id:8, name:'Videojuegos', icon: <IoGameController className={`${iconCategoryClasses}`}/> },
-        { id:9, name:'Vehículos', icon: <IoCar className={`${iconCategoryClasses}`}/> },
-        { id:10, name:'Otros', icon: <IoEllipsisHorizontalSharp className={`${iconCategoryClasses}`}/> }
+        { id:0, name:'Todas', icon: <IoGrid className={`${iconCategoryClasses} ${iconColor(0)}`}/> },
+        { id:1, name:'Deportes', icon: <IoAmericanFootball className={`${iconCategoryClasses} ${iconColor(1)}`}/> },
+        { id:2, name:'Electrónica', icon: <IoPhonePortraitOutline className={`${iconCategoryClasses} ${iconColor(2)}`}/> },
+        { id:3, name:'Hogar', icon: <IoHome className={`${iconCategoryClasses} ${iconColor(3)}`}/> },
+        { id:4, name:'Libros', icon: <IoBookSharp className={`${iconCategoryClasses} ${iconColor(4)}`}/> },
+        { id:5, name:'Oficina', icon: <IoPrint className={`${iconCategoryClasses} ${iconColor(5)}`}/> },
+        { id:6, name:'Películas', icon: <IoFilmSharp  className={`${iconCategoryClasses} ${iconColor(6)}`}/> },
+        { id:7, name:'Ropa', icon: <IoShirt className={`${iconCategoryClasses} ${iconColor(7)}`}/> },
+        { id:8, name:'Videojuegos', icon: <IoGameController className={`${iconCategoryClasses} ${iconColor(8)}`}/> },
+        { id:9, name:'Vehículos', icon: <IoCar className={`${iconCategoryClasses} ${iconColor(9)}`}/> },
+        { id:10, name:'Otros', icon: <IoEllipsisHorizontalSharp className={`${iconCategoryClasses} ${iconColor(10)}`}/> }
     ]
     
-    const [selectedCategory, setSelectedCategory] = useState(0);
-    return (
+    return ( 
         <div className={'bg-[#2E2F2F] w-full h-full min-h-fit py-4 px-10 lg:text-xl md:text-lg sm:text-2xl'}>
             <p className='text-white font-semibold py-2'>
                 Categorías
@@ -42,7 +45,7 @@ export default function Categories()
                 id={category.id} 
                 className={`flex py-0.5 px-2 hover:bg-[#7E868F] border-black rounded-lg cursor-pointer ${selectedCategory === category.id?'bg-[#7E868F]':'bg-[#2E2F2F]'}`}
                 onClick={() => {
-                    console.log(selectedCategory);
+                    //console.log(selectedCategory);
                     setSelectedCategory(category.id);
                     //TODO: REQUEST
                 }}
