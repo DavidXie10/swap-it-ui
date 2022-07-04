2cfc694a5207ad28002d9b9f58fe7c029cd59b20
DavidXie10 <davidxieli@gmail.com>
2022-07-03 22:54:42 -0600

    Added exchange item reducer

--- src/Slices/exchangeItem/exchangeItemSlice.js
+++ src/Slices/exchangeItem/exchangeItemSlice.js
@@ -1,16 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { exchangeItemReducers } from "./reducers";
+import { exchange } from "./requests/exchange";
+
+const onExchangeItemFullfiled = (state, action) => {
+    if (action.payload.error) {
+        state.success = false;
+        state.errorMessage = action.payload.message;
+    } else {
+        state.success = true;
+        state.errorMessage = '';
+    }
+};
+
+const onExchangeItemRejected = (state) => {
+    state.itemToReceive = null;
+    state.itemsToGive = [];
+    state.errorMessage = '';
+    state.success = false;
+}
 
 const exchangeItemSlice = createSlice({
     name: 'exchangeItem',
     initialState: {
         itemToReceive: null,
         itemsToGive: [],
-        errorMessage: ''
+        errorMessage: '',
+        success: false
     },
     reducers: exchangeItemReducers,
+    extraReducers(builder){
+        builder
+            .addCase(exchange.fulfilled, onExchangeItemFullfiled)
+            .addCase(exchange.rejected, onExchangeItemRejected)
+    }
 })
 
-export const { addItemToReceive, toggleItemToGive, clearGiveState, clearState} = exchangeItemSlice.actions
+export const { addItemToReceive, toggleItemToGive, clearGiveState, clearState, setSucess} = exchangeItemSlice.actions
 
 export default exchangeItemSlice.reducer;
