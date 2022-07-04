eb210e2c62370a43f776fd83096053ece0c2f0bb
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-07-04 01:39:19 -0600

    Added image not found in image checkbox

--- src/Components/ImageCheckbox/index.js
+++ src/Components/ImageCheckbox/index.js
@@ -9,7 +9,7 @@ export default function ImageCheckbox ({
                 <div className="absolute top-0 right-0" >
                     <Checkbox onClick={onClick}/>
                 </div>
-                <img src={fileURL || "file not found"} alt={"Foto del artículo a intercambiar"} width={'200px'} height={'80px'} />
+                <img src={fileURL || 'https://ci0137.s3.amazonaws.com/swap-it/uploads/broken-link-g8cf1ab24b_1280.png'} alt={"Foto del artículo a intercambiar"} width={'200px'} height={'80px'} />
             </div>
         </label>
     )
