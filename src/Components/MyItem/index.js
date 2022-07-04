ef9cac7bf39a728bfd56c40001ebaaf535466571
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-07-03 22:36:55 -0600

    Augmented margin top from buttons to text in myItems

--- src/Components/MyItem/index.js
+++ src/Components/MyItem/index.js
@@ -16,8 +16,8 @@ export default function MyItem ({
                 <Label text={`Ubicación: ${address || "Address not found"}`} width={'col-span-3'} height={'row-span-[0.5]'} font={'normal'} textposition={'text-left'} backgroundcolor={backgroundcolor || ""} size={'text-lg'}></Label>
                 <Label text={`Fecha de adquisición: ${acquisition || "Acquisition not found"}`} height={'row-span-1'} width={'col-span-3'} font={'normal'} textposition={'text-left'} backgroundcolor={backgroundcolor || ""} size={'text-lg'}></Label>
                 <Label text={`Busco: ${searchFor || "SearchFor not found"}`} width={'col-span-3'} height={'row-span-1'} font={'normal'} textposition={'text-left'} backgroundcolor={backgroundcolor || ""} size={'text-lg'}></Label>
-                <Button width='lg:w-[180px] md:w-[140px] sm:w-[100px] lg:col-start-3 md:col-start-3 sm:col-start-2 flex justify-center items-center' height={'lg:h-[45px] md:h-[50px] sm:h-[55px] mt-4'} label='Editar' onClick={onClickEdit || ''} textcolor='text-white'/>
-                <Button width='lg:w-[180px] md:w-[140px] sm:w-[100px] lg:col-start-4 md:col-start-4 sm:col-start-4 flex justify-center items-center ml-1 mt-4' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Eliminar' onClick={onClickDelete || ''} backgroundcolor='bg-[#8C8D8D]' textcolor='text-white'/>
+                <Button width='lg:w-[180px] md:w-[140px] sm:w-[100px] lg:col-start-3 md:col-start-3 sm:col-start-2 flex justify-center items-center' height={'lg:h-[45px] md:h-[50px] sm:h-[55px] mt-8'} label='Editar' onClick={onClickEdit || ''} textcolor='text-white'/>
+                <Button width='lg:w-[180px] md:w-[140px] sm:w-[100px] lg:col-start-4 md:col-start-4 sm:col-start-4 flex justify-center items-center ml-1 mt-8' height={'lg:h-[45px] md:h-[50px] sm:h-[55px]'} label='Eliminar' onClick={onClickDelete || ''} backgroundcolor='bg-[#8C8D8D]' textcolor='text-white'/>
             </div>
             <div className={`flex justify-end ${backgroundcolor || ""}`}></div>
         </div>
